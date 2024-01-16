36adf08c0b518864414b4a90a710b64712b81d37
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-16 10:29:06 +0430

    add the commponents

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/greetings/greetingsSlice';
 
 function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting, isLoading, error } = useSelector((state) => state.greetings);
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  },
+  [dispatch]);
+
+  const handleClick = () => {
+    dispatch(fetchRandomGreeting());
+  };
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="loading">...isLoading</div>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <div className="loading">
+          Something is wrong during fetching the data
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-      <h1>Hello, world!</h1>
+      <div className="title">Random Greeting</div>
+      <div className="greeting">{greeting}</div>
+      <button type="button" onClick={handleClick}>Get new greeting</button>
     </>
   );
 }
